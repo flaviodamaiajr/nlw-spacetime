69ae84d04997f2b648eff08f7135d0aed11c8222
Flávio A. da Maia Jr <flaviodamaiajr@gmail.com>
2023-05-21 22:28:11 -0300

    feat: Implement upload.

--- server/src/routes/memories.ts
+++ server/src/routes/memories.ts
@@ -2,7 +2,7 @@ import { FastifyInstance } from 'fastify'
 import { prisma } from '../lib/prisma'
 import { z } from 'zod'
 
-export async function memeoriesRoutes(app: FastifyInstance) {
+export async function memoriesRoutes(app: FastifyInstance) {
   app.addHook('preHandler', async (request) => {
     await request.jwtVerify()
   })
